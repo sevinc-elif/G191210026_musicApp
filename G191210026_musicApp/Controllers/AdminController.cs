b28e98b22930f89aca970b0eccabe4603e2bbf24
sevinc-elif <63056911+sevinc-elif@users.noreply.github.com>
2021-12-19 23:33:32 +0300

    Admin Müzik Listesi

---
+++ G191210026_musicApp/Controllers/AdminController.cs
@@ -0,0 +1,78 @@
+﻿using G191210026_musicApp.Data;
+using G191210026_musicApp.Entity;
+using G191210026_musicApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace G191210026_musicApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly MusicContext _context;
+        public AdminController(MusicContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AdminMusicList()
+        {
+            return View(new AdminMusicsViewModel
+            {
+                Musics = _context.Musics
+                .Include(m => m.Genres)
+                .Select(m => new AdminMusicViewModel
+                {
+                    MusicId = m.MusicId,
+                    MusicName = m.MusicName,
+                    Singer = m.Singer,
+                    ImageUrl = m.ImageUrl,
+                    Genres = m.Genres.ToList()
+                })
+                .ToList()
+            });
+        }
+        public IActionResult MusicCreate()
+        {
+            ViewBag.Genres = _context.Genres.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MusicCreate(Music m,int[] genreIds)
+        {
+            if (ModelState.IsValid)
+            {
+                m.Genres = new List<Genre>();
+                foreach (var id in genreIds)
+                {
+                    m.Genres.Add(_context.Genres.FirstOrDefault(i => i.GenreId == id));
+                }
+                _context.Musics.Add(m);
+                _context.SaveChanges();
+                return RedirectToAction("AdminMusicList","Admin");
+            }
+            ViewBag.Genres = _context.Genres.ToList();
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult MusicDelete(int musicId)
+        {
+            var entity = _context.Musics.Find(musicId);
+            if (entity != null)
+            {
+                _context.Musics.Remove(entity);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("AdminMusicList");
+        }
+
+    }
+}
---
+++ G191210026_musicApp/Models/AdminMusicsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using G191210026_musicApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace G191210026_musicApp.Models
+{
+    public class AdminMusicsViewModel
+    {
+        public List<AdminMusicViewModel> Musics { get; set; }
+    }
+
+    public class AdminMusicViewModel
+    {
+        public int MusicId { get; set; }
+        public string MusicName { get; set; }
+        public string Singer { get; set; }
+        public string ImageUrl { get; set; }
+        public List<Genre> Genres { get; set; }
+    }
+
+    public class AdminEditMusicViewModel
+    {
+        public int MusicId { get; set; }
+        public string MusicName { get; set; }
+        public string Singer { get; set; }
+        
+        public List<Genre> SelectedGenres { get; set; }
+    }
+}
